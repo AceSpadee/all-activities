544262708a96b98c131eb82799bdc249c5a073d8
AceSpadee <148384493+AceSpadee@users.noreply.github.com>
2024-04-22 21:38:21 -0700

    worked on lessons

--- 03-JavaScript/01-Activities/04-Stu_Hello-Variable/Unsolved/script.js
+++ 03-JavaScript/01-Activities/04-Stu_Hello-Variable/Unsolved/script.js
@@ -1,1 +1,20 @@
 // Write Your JavaScript Code Here
+
+//declaring the variables
+let personName;
+
+let pets;
+
+let funFact;
+
+// assigning variables a value
+personName = "Peyton";
+pets = 0;
+funFact = "I am ambidextrous";
+
+
+// calling the variable in the console log
+
+console.log("My name is " + personName);
+console.log(`I have ${pets} pet(s)`);
+console.log("Fun fact: " + funFact);
--- 03-JavaScript/01-Activities/06-Stu_Primitive-Types/Unsolved/script.js
+++ 03-JavaScript/01-Activities/06-Stu_Primitive-Types/Unsolved/script.js
@@ -7,6 +7,11 @@ let six = "Howdy!";
 let seven;
 
 // Insert comments to explain what each console log below will log to the console
+
+// each console log is listing the current value above as what type of value it is
+
+// its listing what type of value it is
+// ex. one is a number, two is a string, three is boolean, four is object, seven is undefiened
 console.log(typeof one); 
 console.log(typeof two); 
 console.log(typeof three); 
@@ -20,6 +25,9 @@ six = false;
 seven = 23;
 
 // Insert comments to explain what each console log below will log to the console
+
+// these console logs overwrite five six and seven from the code above to the new value
+// which is string, boolean, and number
 console.log(typeof five);
 console.log(typeof six);
 console.log(typeof seven);
