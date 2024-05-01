06d0c85017858f0ac625868597088c219c872add
AceSpadee <148384493+AceSpadee@users.noreply.github.com>
2024-04-30 21:51:04 -0700

    worked on assignments

--- 04-Web-APIs/01-Activities/16-Stu_Keyboard-Events/Unsolved/assets/js/script.js
+++ 04-Web-APIs/01-Activities/16-Stu_Keyboard-Events/Unsolved/assets/js/script.js
@@ -1,11 +1,17 @@
 function keydownAction(event) {
-  // TODO: Complete keydown function
+  document.querySelector("#key").textContent = event.key
+  document.querySelector("#code").textContent = event.code
+  document.querySelector("#status").textContent = "KEYDOWN Event";
 }
 
-function keyupAction() {
+function keyupAction(event) {
+  document.querySelector("#key").textContent = event.key
+  document.querySelector("#code").textContent = event.code
+  const key = event.key;
   document.querySelector("#status").textContent = "KEYUP Event";
 }
 
 document.addEventListener("keyup", keyupAction);
 // TODO: Add Event Listener for "keydown" event
-
+  
+document.addEventListener("keydown", keydownAction);
