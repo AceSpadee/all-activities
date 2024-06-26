bbf216095e7527ca65e86bb1cebd2d43b2140e2b
AceSpadee <148384493+AceSpadee@users.noreply.github.com>
2024-06-25 21:39:54 -0700

    worked on assingments

---
+++ 12-SQL/01-Activities/04-Stu_Databases/Unsolved/schema.sql
@@ -0,0 +1,2 @@
+-- Write code here or enter directly in PostgreSQL shell --
+CREATE DATABASE books_db;
---
+++ 12-SQL/01-Activities/06-Stu_Tables/Unsolved/db/schema.sql
@@ -0,0 +1,10 @@
+DROP DATABASE IF EXISTS books_db;
+-- Creates the "books_db" database --
+CREATE DATABASE books_db;
+\c books_db;
+
+-- Add biography table --
+CREATE TABLE biography (
+    id INTEGER,
+    name VARCHAR(100)
+)
